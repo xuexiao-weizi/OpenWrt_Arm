554c55b773f1ad6a2e9e282f71737a4946decb48
xuexiao-weizi <78170980+xuexiao-weizi@users.noreply.github.com>
2021-03-10 09:08:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,10 +16,6 @@
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-rm -rf package/lean/luci-theme-argon
-
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus	
 git clone https://github.com/vernesong/OpenClash package/luci-app-openclash	
