7a0976fb152d4edfdf801d19b4b9bd1c10df2285
xuexiao-weizi <78170980+xuexiao-weizi@users.noreply.github.com>
2021-05-22 08:22:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,10 +22,10 @@ git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr	
 git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall	
-git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass	
+#git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass	
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/tcping package/tcping	
-find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}	
-find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
+#find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}	
+#find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
 
 
 #添加smartdns
